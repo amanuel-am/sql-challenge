7ecd6355d5b8123d232dca4d67ef90131b215a88
amaniz1st@gmail.com <amaniz1st@gmail.com>
2024-05-16 17:03:57 -0500

    queries and schemata

--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -1,3 +1,83 @@
+--CREATE DEPARTMENTS TABLE---------------------------------
+
+CREATE TABLE departments(
+	dept_no VARCHAR(5) PRIMARY KEY,
+	dept_name VARCHAR(50)
+);
+
+-----------------------------------------------------------
+SELECT * FROM departments
+
+-- CREATE EMPLOYEE TABLE-----------------------------------
+
+CREATE TABLE employees(
+	emp_no int NOT NULL PRIMARY KEY,
+	emp_title VARCHAR(50)NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50)NOT NULL,
+	last_name VARCHAR(50)NOT NULL,
+	sex VARCHAR(5) NOT NULL,
+	hire_date DATE NOT NULL
+	
+);
+
+-------------------------------------------------------------
+SELECT * FROM employees
+
+
+-- CREATE DEPT_EMP TABLE-------------------------------------
+
+
+CREATE TABLE dept_emp(
+	dept_emp_id SERIAL PRIMARY KEY,
+	emp_no int NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-----------------------------------------------------------------
+SELECT * FROM dept_emp
+
+
+-- CREATE DEPT_MANAGER TABLE-------------------------------------
+
+CREATE TABLE dep_manager(
+	dept_manager_id SERIAL PRIMARY KEY,
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no int NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)	
+);
+
+-------------------------------------------------------------------
+SELECT * FROM dep_manager;
+
+
+-- CREATE SALARIES TABLE-------------------------------------------
+
+CREATE TABLE salaries(
+	salary_id SERIAL PRIMARY KEY,
+	emp_no int NOT NULL,
+	salary int NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)	
+);
+
+-------------------------------------------------------------------
+SELECT * FROM dept_emp
+
+
+-- CREATE TITLES TABLE-------------------------------------------
+
+CREATE TABLE titles(
+	title_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL	
+);
+
+-------------------------------------------------------------------
+SELECT * FROM titles
+
+
 
 -- Q1-----------------------------------------------------------------
 
